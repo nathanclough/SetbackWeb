3223d8c967c2c91975301bf0b809f0c479b9ac0b
Nathan <nathan.clough@ttu.edu>
2021-12-01 17:30:43 -0600

    commmunication by events

---
+++ app.py
@@ -0,0 +1,21 @@
+import socketio
+
+sio = socketio.AsyncServer(async_mode='asgi')
+app = socketio.ASGIApp(sio,static_files={
+    '/':'./public/'
+})
+
+@sio.event
+async def connect(sid,environ):
+    print(sid,'connected')
+
+@sio.event
+async def disconnected(sid):
+    print(sid,'disconnected')
+
+@sio.event
+async def sum(sid,data):
+    result = data['numbers'][0] + data['numbers'][1]
+    await sio.emit('sum_result',{'result':result},to=sid)
+    print(sid,data)
+
