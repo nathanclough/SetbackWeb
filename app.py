07363cceccf17a2aa3a38d8dde06ee27da734edc
Nathan <nathan.clough@ttu.edu>
2021-12-01 17:33:43 -0600

    implicit response to event using call back

--- app.py
+++ app.py
@@ -16,6 +16,5 @@ async def disconnected(sid):
 @sio.event
 async def sum(sid,data):
     result = data['numbers'][0] + data['numbers'][1]
-    await sio.emit('sum_result',{'result':result},to=sid)
-    print(sid,data)
+    return result 
 
--- public/index.js
+++ public/index.js
@@ -2,7 +2,9 @@ const sio = io();
 
 sio.on('connect', () => {
   console.log('connected');
-  sio.emit('sum',{numbers: [1,2]});
+  sio.emit('sum',{numbers: [1,2]}, (result) => {
+      console.log(result)
+  } );
 });
 
 sio.on('disconnect', () => {
